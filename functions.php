5e6df1a8df0cfb38f75ecb7c5bc2a0483ab2a927
Dave Naluz <dave@playgroundinc.com>
2019-08-16 14:15:44 -0400

    Removed admin bar from homepage and added theme css file

--- functions.php
+++ functions.php
@@ -1,2 +1,12 @@
 <?php
-    // Mozilla theme functions file
+// Mozilla theme functions file
+
+// Remove the admin header styles for homepage
+add_action('get_header', 'remove_admin_login_header');
+
+// Include theme style.css file 
+wp_enqueue_style('style', get_stylesheet_uri());
+
+function remove_admin_login_header() {
+	remove_action('wp_head', '_admin_bar_bump_cb');
+}
