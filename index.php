a5f4584e150e37446bee2bb036ad38f66eab7822
Dave Naluz <dave@playgroundinc.com>
2019-10-08 17:04:17 -0400

    Added to display page content

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php get_header(); ?>
-    <div class="content">
 
+
+
+
+
+    <div class="content">
+    <?php if ( have_posts() ) : ?>
+        <?php while ( have_posts() ) : the_post(); ?>
+
+
+            <?php print_r(the_post()); ?>
+
+
+          <?php the_content() ?>
+        <?php endwhile; ?>
+      <?php endif; ?>
     </div>
 <?php get_footer(); ?>
