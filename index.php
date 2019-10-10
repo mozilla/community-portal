b534e38b37db0c2710b9883fef8b86fd97005c31
Dave Naluz <dave@playgroundinc.com>
2019-10-10 11:54:47 -0400

    spacing

--- index.php
+++ index.php
@@ -1,17 +1,7 @@
 <?php get_header(); ?>
-
-
-
-
-
     <div class="content">
     <?php if ( have_posts() ) : ?>
         <?php while ( have_posts() ) : the_post(); ?>
-
-
-            <?php print_r(the_post()); ?>
-
-
           <?php the_content() ?>
         <?php endwhile; ?>
       <?php endif; ?>
