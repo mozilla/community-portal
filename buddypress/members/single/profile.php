5a99d21ce915a915740f7cedf63dd5454c496a12
Dave Naluz <dave@playgroundinc.com>
2019-10-16 14:51:57 -0400

    Added edit for mobile

--- buddypress/members/single/profile.php
+++ buddypress/members/single/profile.php
@@ -14,6 +14,17 @@
 <div class="profile__public-container">
     <section class="profile__card">
         <div class="profile__card-header-container">
+            <?php if($is_me): ?>
+            <div class="profile__edit-link-container profile__edit-link-container--mobile">
+                <a href="/members/<?php print $user->user_nicename; ?>/profile/edit/group/1" class="profile__link">
+                <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg" class="profile__edit-icon">
+                    <path d="M8.25 3H3C2.60218 3 2.22064 3.15804 1.93934 3.43934C1.65804 3.72064 1.5 4.10218 1.5 4.5V15C1.5 15.3978 1.65804 15.7794 1.93934 16.0607C2.22064 16.342 2.60218 16.5 3 16.5H13.5C13.8978 16.5 14.2794 16.342 14.5607 16.0607C14.842 15.7794 15 15.3978 15 15V9.75" stroke="#0060DF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M13.875 1.87419C14.1734 1.57582 14.578 1.4082 15 1.4082C15.422 1.4082 15.8266 1.57582 16.125 1.87419C16.4234 2.17256 16.591 2.57724 16.591 2.99919C16.591 3.42115 16.4234 3.82582 16.125 4.12419L9 11.2492L6 11.9992L6.75 8.99919L13.875 1.87419Z" stroke="#0060DF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+                <?php print __('Edit'); ?>
+                </a>
+            </div>
+            <?php endif; ?>
             <div class="profile__avatar<?php if($avatar === false): ?> profile__avatar--empty<?php endif; ?>" <?php if($is_me && $avatar || $logged_in && $avatar): ?>style="background-image: url('<?php print $avatar; ?>')"<?php endif; ?>></div>
             <div class="profile__name-container">
                 <h3 class="profile__user-title"><?php print $user->user_nicename; ?></h3>
