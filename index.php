058fbf5ac5c4cf6537995966a48915756805189f
Ky Capstick <ky.capstick@gmail.com>
2019-10-22 16:03:18 -0400

    Fix styles for index phpo

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-    <div class="content container">
+    <div class="content">
     <?php if ( have_posts() ) : ?>
         <?php while ( have_posts() ) : the_post(); ?>
           <?php the_content() ?>
