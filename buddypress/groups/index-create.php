98850f7d092e26cdcd44f393e7d3b17fffbdb88b
Dave Naluz <dave@playgroundinc.com>
2019-11-06 15:23:37 -0500

    Reorganized how group image uploader works

--- buddypress/groups/index-create.php
+++ buddypress/groups/index-create.php
@@ -89,14 +89,15 @@
                     </div>
                     <div class="create-group__input-container create-group__input-container--short">
                         <label class="create-group__label" for="group-desc"><?php print __("Group Photo"); ?></label>
-                        <div id="group-photo-uploader" class="create-group__image-upload">
+                        <div id="group-photo-uploader" class="create-group__image-upload<?php if(isset($form['image_url']) && strlen($form['image_url']) > 0): ?> create-group__image-upload--done<?php endif; ?>"<?php if(isset($form['image_url'])): ?> style="background-image: url('<?php print $form['image_url'];?>')"<?php endif; ?>>
                             <svg width="75" height="75" viewBox="0 0 75 75" fill="none" xmlns="http://www.w3.org/2000/svg" class="create-group__upload-image-svg">
                                 <path d="M59.375 9.375H15.625C12.1732 9.375 9.375 12.1732 9.375 15.625V59.375C9.375 62.8268 12.1732 65.625 15.625 65.625H59.375C62.8268 65.625 65.625 62.8268 65.625 59.375V15.625C65.625 12.1732 62.8268 9.375 59.375 9.375Z" stroke="#CDCDD4" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                                 <path d="M26.5625 31.25C29.1513 31.25 31.25 29.1513 31.25 26.5625C31.25 23.9737 29.1513 21.875 26.5625 21.875C23.9737 21.875 21.875 23.9737 21.875 26.5625C21.875 29.1513 23.9737 31.25 26.5625 31.25Z" stroke="#CDCDD4" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                                 <path d="M65.625 46.875L50 31.25L15.625 65.625" stroke="#CDCDD4" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                             </svg>
                         </div>
-                        <div class="create-group__image-instructions"><?php print __("Click or drag a photo above"); ?></div>
+                        <a class="dz-remove<?php if(!isset($form['image_url']) || strlen($form['image_url']) === 0): ?> dz-remove--hide<?php endif; ?>" href="#" data-dz-remove="" >Remove file</a>
+                        <div class="create-group__image-instructions<?php if(isset($form['image_url']) && strlen($form['image_url']) > 0): ?> create-group__image-instructions--hide<?php endif; ?>"><?php print __("Click or drag a photo above"); ?></div>
                         <input type="hidden" name="image_url" id="image-url" value="<?php print (isset($form['image_url'])) ? $form['image_url'] : '' ?>" />
                     </div>
                     <div class="create-group__input-container create-group__input-container--full">
