75a211b64aef852849032ed4abedbf1e003c9180
Ky Capstick <ky.capstick@gmail.com>
2019-11-15 16:05:56 -0500

    Add conditional logic for location

--- plugins/events-manager/templates/template-parts/single-event-card.php
+++ plugins/events-manager/templates/template-parts/single-event-card.php
@@ -28,6 +28,8 @@
       <div class="event-card__description">
         <h3 class="event-card__description__title title--event-card"><?php echo $event->event_name; ?></h2>
         <p><?php echo __($months[$month].' '.$date.', '.$year.' @ '.substr($event->event_start_time, 0, 5).' - '.substr($event->event_end_time, 0, 5).' '.$event->event_timezone); ?></p>
+        <?php if (strlen($location->address) > 0 || strlen($location->town) > 0 || strlen($location->country) > 0):
+        ?>
         <div class="event-card__location">
           <svg width="16" height="18" viewBox="0 0 16 18" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M14 7.66602C14 12.3327 8 16.3327 8 16.3327C8 16.3327 2 12.3327 2 7.66602C2 6.07472 2.63214 4.54859 3.75736 3.42337C4.88258 2.29816 6.4087 1.66602 8 1.66602C9.5913 1.66602 11.1174 2.29816 12.2426 3.42337C13.3679 4.54859 14 6.07472 14 7.66602Z" stroke="#737373" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
@@ -49,6 +51,9 @@
             ?>
           </p>
         </div>
+        <?php 
+          endif;
+        ?>
       </div>
       <ul class="events__tags">
         <?php
