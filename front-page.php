80af8a99d43d2fa422aee7f20e03bd478070ef62
Ky Capstick <ky.capstick@gmail.com>
2019-11-21 15:54:09 -0500

    Pull in featured events

--- front-page.php
+++ front-page.php
@@ -7,14 +7,15 @@
           'hero_image',
           'hero_cta_existing',
           'hero_cta_new',
-          'hero_cta_text'
+          'hero_cta_text',
+          'featured_events'
         );
         $fieldValues = new stdClass();
         foreach ($fields as $field) {
           $fieldValues->$field = get_field($field);
         }
       ?>
-      <div class="homepage container">
+      <div class="homepage homepage__container">
         <div class="row homepage__hero">
           <div class="col-md-4 col-sm-offset-1 homepage__hero__image">
             <img src="<?php echo $fieldValues->hero_image['url'] ?>" alt="<?php echo $fieldValues->hero_image['alt'] ?>">
@@ -24,6 +25,15 @@
             <p class="homepage__hero__subtitle subtitle"><?php echo $fieldValues->hero_subtitle ?></p>
             <a href="<?php echo (is_user_logged_in() ? esc_attr($fieldValues->hero_cta_existing) : esc_attr($fieldValues->hero_cta_new)) ?>"class="btn btn--dark btn--submit btn--small homepage__hero__cta"><?php echo __($fieldValues->hero_cta_text) ?></a>
           </div>
+        </div>
+        <div class="row homepage__events">
+          <?php 
+            foreach($fieldValues->featured_events as $featured_event):
+              $event = EM_Events::get(array('post_id' => $featured_event['single_event']->ID));
+              $event = $event[0];
+              include(locate_template('plugins/events-manager/templates/template-parts/single-event-card.php', false, false));
+            endforeach;
+          ?>
         </div>
       </div>
     </div>
