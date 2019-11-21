5990c4c07dfbbe4ee604e20a5aa75fb8b8584dfe
Ky Capstick <ky.capstick@gmail.com>
2019-11-21 11:13:38 -0500

    Add fields for hero CTA links for new and existing users and hero cta text, update functionality to iterate through fields

--- front-page.php
+++ front-page.php
@@ -1,19 +1,28 @@
 <?php get_header(); ?>
     <div class="content">
       <?php 
-        $heroTitle = get_field('hero_title');
-        $heroSubTitle = get_field('hero_subtitle');
-        $heroImage = get_field('hero_image');
+        $fields = array(
+          'hero_title',
+          'hero_subtitle',
+          'hero_image',
+          'hero_cta_existing',
+          'hero_cta_new',
+          'hero_cta_text'
+        );
+        $fieldValues = new stdClass();
+        foreach ($fields as $field) {
+          $fieldValues->$field = get_field($field);
+        }
       ?>
       <div class="homepage container">
         <div class="row homepage__hero">
           <div class="col-md-4 col-sm-offset-1 homepage__hero__image">
-            <img src="<?php echo $heroImage['url'] ?>" alt="<?php echo $heroImage['alt'] ?>">
+            <img src="<?php echo $fieldValues->hero_image['url'] ?>" alt="<?php echo $fieldValues->hero_image['alt'] ?>">
           </div>
           <div class="col-md-5 col-md-offset-1">
-            <h1 class="homepage__hero__title title title--main"><?php echo preg_replace('/\b(Community)\b/i', '<span>$0</span>', $heroTitle); ?></h1>
-            <p class="homepage__hero__subtitle subtitle"><?php echo $heroSubTitle ?></p>
-            <a class="btn btn--dark btn--submit btn--small homepage__hero__cta"><?php echo __('Get Started') ?></a>
+            <h1 class="homepage__hero__title title title--main"><?php echo preg_replace('/\b(Community)\b/i', '<span>$0</span>', $fieldValues->hero_title); ?></h1>
+            <p class="homepage__hero__subtitle subtitle"><?php echo $fieldValues->hero_subtitle ?></p>
+            <a href="<?php echo (is_user_logged_in() ? esc_attr($fieldValues->hero_cta_existing) : esc_attr($fieldValues->hero_cta_new)) ?>"class="btn btn--dark btn--submit btn--small homepage__hero__cta"><?php echo __($fieldValues->hero_cta_text) ?></a>
           </div>
         </div>
       </div>
