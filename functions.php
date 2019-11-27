7a6e0ec1a72668cda0f8152eaee641d7be69d3a2
Dave Naluz <dave@playgroundinc.com>
2019-11-27 14:52:28 -0500

    Fixed bug where group creator wasn't allowing you to create a secondary admin

--- functions.php
+++ functions.php
@@ -505,8 +505,12 @@ function mozilla_create_group() {
                                 }
 
                                 if(isset($_POST['group_admin_id']) && $_POST['group_admin_id']) {
-                                    groups_join_group($group_id, intval($_POST['group_admin_id']));
-                                    groups_promote_member(intval($_POST['group_admin_id']), $group_id, 'admin');
+                                    $group_admin_user_id = intval($_POST['group_admin_id']);
+
+                                    groups_join_group($group_id, $group_admin_user_id);
+                                    $member = new BP_Groups_Member($group_admin_user_id, $group_id); 
+                                    do_action('groups_promote_member', $group_id, $group_admin_user_id, 'admin'); 
+                                    $member->promote('admin'); 
                                 }
 
                                 // Required information but needs to be stored in meta data because buddypress does not support these fields
