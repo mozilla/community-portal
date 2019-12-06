7288e69fcfd029e8e2b5fa027817ca2c665e647d
Dave Naluz <dave@playgroundinc.com>
2019-12-06 15:16:51 -0500

    Fixed typo

--- plugins/events-manager/templates/my-bookings.php
+++ plugins/events-manager/templates/my-bookings.php
@@ -30,7 +30,7 @@
         </div>
 <?php } ?>
     <?php else: ?>
-        <p><?php echo sprintf(__('Please log n to create or join events','events-manager'),site_url('wp-login.php?redirect_to=' . urlencode(get_permalink()), 'login'))?></p>
+        <p><?php echo sprintf(__('Please log in to create or join events','events-manager'),site_url('wp-login.php?redirect_to=' . urlencode(get_permalink()), 'login'))?></p>
     <?php endif; ?>
     </div>
 
