c621807336150812e7749a757360067c0c7a390a
Dave Naluz <dave@playgroundinc.com>
2019-12-06 17:23:52 -0500

    Completely refactored how events search section is rendered

--- plugins/events-manager/templates/events-search.php
+++ plugins/events-manager/templates/events-search.php
@@ -1,33 +1,27 @@
 <?php
-/* WARNING!!! (2013-07-10) We intend to add a few more fields into this search form over the coming weeks/months. 
- * Overriding shouldn't hinder functionality at all but these new search options won't appear on your form! 
- */ 
-/* 
- * By modifying this in your theme folder within plugins/events-manager/templates/events-search.php, you can change the way the search form will look.
- * To ensure compatability, it is recommended you maintain class, id and form name attributes, unless you now what you're doing. 
- * You also have an $args array available to you with search options passed on by your EM settings or shortcode
- */
-$args = !empty($args) ? $args:array(); /* @var $args array */
+
+	$args = !empty($args) ? $args:array(); /* @var $args array */
 ?>
+
 <div class="em-search-wrapper row">
-<div class="em-events-search em-search col-lg-7 col-sm-12 css-search">
-  <?php 
-    $view = get_query_var( 'view', $default = '');
-    $args['search_url'] = '/events/?view='.$view;
-  ?>
-	<form action="<?php echo !empty($args['search_url']) ? esc_url($args['search_url']) : EM_URI; ?>" method="post" class="em-events-search-form em-search-form">
-		<input type="hidden" name="action" value="<?php echo esc_attr($args['search_action']); ?>" />
-		<div class="em-search-main">
+	<div class="em-events-search em-search col-lg-7 col-sm-12">
+		<?php 
+			$view = get_query_var( 'view', $default = '');
+			$args['search_url'] = '/events/?view='.$view;
+		?>
+		<form method="POST" action="<?php echo !empty($args['search_url']) ? esc_url($args['search_url']) : EM_URI; ?>" class="events__form">
+			<input type="hidden" name="action" value="<?php echo esc_attr($args['search_action']); ?>" />
 			<?php do_action('em_template_events_search_form_header'); //hook in here to add extra fields, text etc. ?>
-			<?php 
-			//search text
-			if( !empty($args['search_term']) ) em_locate_template('templates/search/search.php',true,array('args'=>$args));
-			?>
-			<?php if( !empty($args['css']) ) : //show the button here if we're using the default styling, if you still want to use this and use custom CSS, then you have to override our rules ?>
-			<button type="submit" class="em-search-submit loading">Search</button>
-			<?php endif; ?>
-		</div>
-	</form>
+			<div class="events__input-container">
+			<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<path d="M9.16667 15.8333C12.8486 15.8333 15.8333 12.8486 15.8333 9.16667C15.8333 5.48477 12.8486 2.5 9.16667 2.5C5.48477 2.5 2.5 5.48477 2.5 9.16667C2.5 12.8486 5.48477 15.8333 9.16667 15.8333Z" stroke="#737373" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+				<path d="M17.5 17.5L13.875 13.875" stroke="#737373" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+			</svg>
+			<input type="text" name="em_search" class="events__search-input" placeholder="<?php print __("Search events"); ?>" value="<?php echo esc_attr($args['search']); ?>" />
+			</div>
+			<input type="submit" class="events__search-cta" value="<?php print __("Search"); ?>" />
+		</form>
+	</div>
+	<?php if( !empty($args['ajax']) ): ?><div class='em-search-ajax'></div><?php endif; ?>
 </div>
-<?php if( !empty($args['ajax']) ): ?><div class='em-search-ajax'></div><?php endif; ?>
-</div>
+
