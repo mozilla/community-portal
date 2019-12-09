402ca54d73a290113e521fb2f537feff379fe409
Dave Naluz <dave@playgroundinc.com>
2019-12-09 14:55:26 -0500

    Removed unused code

--- plugins/events-manager/templates/events-list.php
+++ plugins/events-manager/templates/events-list.php
@@ -1,15 +1,4 @@
 <?php
-    /*
-     * Default Events List Template
-     * This page displays a list of events, called during the em_content() if this is an events list page.
-     * You can override the default display settings pages by copying this file to yourthemefolder/plugins/events-manager/templates/ and modifying it however you need.
-     * You can display events however you wish, there are a few variables made available to you:
-     *
-     * $args - the args passed onto EM_Events::output()
-     *
-     */
-?>
-<?php
     $page = $_REQUEST['pno'];
     $args = apply_filters('em_content_events_args', $args);
     $args['pagination'] = '1';
