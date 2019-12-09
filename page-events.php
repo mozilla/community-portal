040542cbf968a96c8d4a5dd32f859d47569bfbbd
Dave Naluz <dave@playgroundinc.com>
2019-12-09 14:55:10 -0500

    Updated copy

--- page-events.php
+++ page-events.php
@@ -1,20 +1,18 @@
 <?php get_header(); ?>
-  <div class="events__header">
-    <div class="row middle-md events__container">
-      <div class="col-md-6 events__header__text">
-        <h1 class="events__title"><?php the_title() ?></h1>
-        <p class="events__text">A short paragraph about why events are great and why people should look for some near them.</p>
-        <p class="events__text">Fine and event near you below, or <a href="<?php echo add_query_arg(array('action' => 'edit'), get_site_url('','events/edit-event'))?>">create an event</a></p>
-      </div>
+    <div class="events__header">
+        <div class="row middle-md events__container">
+            <div class="col-md-6 events__header__text">
+                <h1 class="events__title"><?php the_title() ?></h1>
+                <p class="events__text"><?php print __("Ready to join the movement? Check out what's happening soon in your area. "); ?></p>
+                <p class="events__text"><?php print __("Explore community events near you, "); ?><a href="<?php echo add_query_arg(array('action' => 'edit'), get_site_url('','events/edit-event'))?>"><?php print __("organize your own!"); ?></a></p>
+            </div>
+        </div>
     </div>
-  </div>
-  <div 
-    class="content events__container"
-  >
-    <?php if ( have_posts() ) : ?>
-      <?php while ( have_posts() ) : the_post(); ?>
-        <?php the_content() ?>
-      <?php endwhile; ?>
-    <?php endif; ?>
-  </div>
+    <div class="content events__container">
+        <?php if(have_posts()) : ?>
+            <?php while ( have_posts() ) : the_post(); ?>
+                <?php the_content() ?>
+            <?php endwhile; ?>
+        <?php endif; ?>
+    </div>
 <?php get_footer(); ?>
