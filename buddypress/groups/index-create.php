69a4f686328c22a1a46f3c6367c1e8bea3b63ac1
Dave Naluz <dave@playgroundinc.com>
2019-12-10 10:21:31 -0500

    Renamed title from admin to contact

--- buddypress/groups/index-create.php
+++ buddypress/groups/index-create.php
@@ -184,7 +184,7 @@
                     </section>
                     <section class="create-group__details<?php if($step == 1): ?> create-group__details--hidden<?php endif; ?>">
                         <div class="create-group__section-title">
-                            <?php print __("Secondary Group Contact *"); ?>
+                            <?php print __("Secondary Group Contact"); ?>
                             <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <title>Secondary group contact</title>
                                 <path d="M9 16.5C13.1421 16.5 16.5 13.1421 16.5 9C16.5 4.85786 13.1421 1.5 9 1.5C4.85786 1.5 1.5 4.85786 1.5 9C1.5 13.1421 4.85786 16.5 9 16.5Z" stroke="#CDCDD4" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
