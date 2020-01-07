4a0b63c91f9fd4beae21f81c991554f1decf7116
Dave Naluz <dave@playgroundinc.com>
2020-01-07 13:53:06 -0500

    Added activities share

--- js/lightbox.js
+++ js/lightbox.js
@@ -41,63 +41,68 @@ jQuery(function() {
     }
 
     function trapFocus($closeBtn, $lastBtn) {
-      $closeBtn.on("keydown", e => {
-          if (e.keyCode === 9 && e.shiftKey) {
-              e.preventDefault();
-              $lastBtn.focus();
-          }
-      });
+        $closeBtn.on("keydown", e => {
+            if (e.keyCode === 9 && e.shiftKey) {
+                e.preventDefault();
+                $lastBtn.focus();
+            }
+        });
 
-      $lastBtn.on("keydown", function(e) {
-          if (e.keyCode === 9 && !e.shiftKey) {
-              e.preventDefault();
-              $closeBtn.focus();
-          }
-      });
+        $lastBtn.on("keydown", function(e) {
+            if (e.keyCode === 9 && !e.shiftKey) {
+                e.preventDefault();
+                $closeBtn.focus();
+            }
+        });
     }
 
     function handleCopyToClipboardClick() {
-      var $copyTrigger = jQuery("#copy-share-link");
-      $copyTrigger.on("click", function(e) {
-          e.preventDefault();
-           
-          copyToClipboard();
-          $copyTrigger.text("Link Copied");
-      });
+        var $copyTrigger = jQuery("#copy-share-link");
+        $copyTrigger.on("click", function(e) {
+            e.preventDefault();
+            
+            copyToClipboard();
+            $copyTrigger.text("Link Copied");
+        });
     }
 
     function copyToClipboard() {
-      var el = document.createElement("textarea");
-      el.value = location.href;
-      el.setAttribute("readonly", "");
-      el.style.position = "absolute";
-      el.style.left = "-9999px";
-      document.body.appendChild(el);
-      el.select();
-      document.execCommand("copy");
-      document.body.removeChild(el);
+        var el = document.createElement("textarea");
+        el.value = location.href;
+        el.setAttribute("readonly", "");
+        el.style.position = "absolute";
+        el.style.left = "-9999px";
+        document.body.appendChild(el);
+        el.select();
+        document.execCommand("copy");
+        document.body.removeChild(el);
     }
 
     function initLightbox() {
-      toggleLightbox(
-        "#attendees-lightbox",
-        "#open-attendees-lightbox",
-        "#close-attendees-lightbox",
-        ".events-single__member-card a"
-      );
-      toggleLightbox(
-          "#events-share-lightbox",
-          "#open-events-share-lightbox",
-          "#close-share-lightbox",
-          "a"
-      );
-      toggleLightbox(
-        "#groups-share-lightbox",
-        ".group__share-cta",
-        "#close-share-lightbox",
-        "a"
-      )
-      handleCopyToClipboardClick();
+        toggleLightbox(
+            "#attendees-lightbox",
+            "#open-attendees-lightbox",
+            "#close-attendees-lightbox",
+            ".events-single__member-card a"
+        );
+
+        toggleLightbox(
+            "#events-share-lightbox",
+            "#open-events-share-lightbox",
+            "#close-share-lightbox",
+            "a"
+        );
+
+        toggleLightbox(
+            "#groups-share-lightbox",
+            ".group__share-cta",
+            "#close-share-lightbox",
+            "a"
+        );
+
+        toggleLightbox('#activity-share-lightbox', '.activity__cta--share', "#close-share-lightbox", 'a');
+
+        handleCopyToClipboardClick();
     }
 
     initLightbox();
