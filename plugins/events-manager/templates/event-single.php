1fb47e3f61b3895299937763753f643024a3d721
Dave Naluz <dave@playgroundinc.com>
2020-02-05 10:40:34 -0500

    Added link in body

--- plugins/events-manager/templates/event-single.php
+++ plugins/events-manager/templates/event-single.php
@@ -381,7 +381,7 @@
 </div>
 <?php if(isset($options['report_email'])): ?>
 <div class="events-single__report-container">
-    <a href="mailto:<?php print $options['report_email']; ?>?subject=<?php print sprintf('%s %s - %s', __('Reporting Event', 'community-portal'), $group->name, "https://{$_SERVER[HTTP_HOST]}{$_SERVER[REQUEST_URI]}" ); ?>&body=<?php print __('Please provide a reason you are reporting this event', 'community-portal'); ?>" class="events-single__report-group-link">
+    <a href="mailto:<?php print $options['report_email']; ?>?subject=<?php print sprintf('%s %s', __('Reporting Event', 'community-portal'), $group->name); ?>&body=<?php print sprintf('%s %s', __('Please provide a reason you are reporting this event', 'community-portal'), "https://{$_SERVER[HTTP_HOST]}{$_SERVER[REQUEST_URI]}"); ?>" class="events-single__report-group-link">
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="#0060DF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
             <path d="M12 8V12" stroke="#0060DF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
