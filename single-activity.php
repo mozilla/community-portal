38ac480e0746ddd7108201936c6b737f2ab1090a
Dave Naluz <dave@playgroundinc.com>
2020-02-05 15:24:27 -0500

    Added check for tags to make sure it's an array

--- single-activity.php
+++ single-activity.php
@@ -107,7 +107,7 @@
                     <div class="activity__right-column">
                         <div class="activity__card">
                             <div class="activity__card-content">
-                                <?php if(sizeof($tags) > 0): ?>
+                                <?php if(sizeof($tags) > 0 && is_array($tags)): ?>
                                 <span><?php print __("Tags"); ?></span>
                                 <div class="activity__tags">
                                 <?php foreach($tags AS $tag): ?>
