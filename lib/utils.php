a61534b12bc70a0fc290343395275fd8abe112a1
Ky Capstick <ky.capstick@gmail.com>
2020-02-11 15:46:37 -0500

    Update front end rendering to check for valid URLs before rendering commmunity links

--- lib/utils.php
+++ lib/utils.php
@@ -255,4 +255,23 @@ function mozilla_redirect_admin() {
     }
 }
 
+function mozilla_verify_url($url, $secure) {
+	if (preg_match('/\.[a-zA-Z]{2,4}\b/', $url)) {
+		$parts = parse_url($url);
+		if (!isset($parts["scheme"])) {
+			if ($secure) {
+				$url = 'https://'.$url;
+			} else {
+				$url = 'http://'.$url;
+
+			}
+		}
+	}
+	if (filter_var($url, FILTER_VALIDATE_URL)) {
+		return $url;
+	}
+	return false;
+}
+
+
 ?>
