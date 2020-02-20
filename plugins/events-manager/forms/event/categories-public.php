13a25b65b6b6db9e7f0de2292c03323bfb4db16a
Ky Capstick <ky.capstick@gmail.com>
2020-02-20 13:53:07 -0500

    Updating events to only display one tag

--- plugins/events-manager/forms/event/categories-public.php
+++ plugins/events-manager/forms/event/categories-public.php
@@ -1,34 +1,33 @@
 <?php
-  global $EM_Event;
-  $categories = EM_Categories::get(array('orderby'=>'name','hide_empty'=>0));
+	global $EM_Event;
+	$categories = EM_Categories::get(array('orderby'=>'name','hide_empty'=>0));
 ?>
-<?php if( count($categories) > 0 ): 
-  ?>
-  <div class="event-categories event-creator__container">
-    <!-- START Categories -->
-    <fieldset class="event-creator__fieldset" id="event_categories[]">
-      <legend class="event-creator__label" for="event_categories[]"><?php print __( 'Select tag(s) for your event', 'commuity-portal'); ?></legend>
-        <?php
-          $selected = $EM_Event->get_categories()->get_ids();
-          foreach($categories as $category) {
-          ?>
-          <input 
-            name="event_categories[]" 
-            class="event-creator__checkbox" 
-            id="<?php echo esc_attr($category->id) ?>"
-            type="checkbox"  
-            value="<?php echo esc_attr($category->id) ?>"
-            <?php if (in_array($category->id, $selected)) {
-              echo esc_attr('checked'); 
-            } 
-            ?>
-          />
-          <label class="event-creator__tag" for="<?php echo esc_attr($category->id)?>"><?php echo __($category->name) ?></label>
-          <?php
-          }
-        ?>
+<?php if( count($categories) > 0 ): ?>
+	<div class="event-categories event-creator__container">
+	<!-- START Categories -->
+		<fieldset class="event-creator__fieldset" id="event_categories[]">
+			<legend class="event-creator__label" for="event_categories[]"><?php print __( 'Select tag(s) for your event', 'commuity-portal'); ?></legend>
+			<?php
+				$selected = $EM_Event->get_categories()->get_ids();
+				foreach($categories as $category) {
+			?>
+				<input 
+					name="event_categories[]" 
+					class="event-creator__checkbox" 
+					id="<?php echo esc_attr($category->id) ?>"
+					type="radio"  
+					value="<?php echo esc_attr($category->id) ?>"
+					<?php if (is_array($selected) && intval($category->id) === intval($selected[0])) {
+						echo esc_attr('checked'); 
+					} 
+					?>
+				/>
+				<label class="event-creator__tag" for="<?php echo esc_attr($category->id)?>"><?php echo __($category->name) ?></label>
+			<?php
+				}
+			?>
         <!-- <input type="hidden" name="event_categories[]" id="event_categories--all" value=""> -->
-      <!-- END Categories -->
-    </fieldset>
-  </div>
+		<!-- END Categories -->
+		</fieldset>
+	</div>
 <?php endif; ?>
