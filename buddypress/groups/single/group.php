21827a891a32b1b04831f7eafc04cc29fd49dc13
Ky Capstick <ky.capstick@gmail.com>
2020-02-20 11:21:25 -0500

    Update url of groups events link to point to events view

--- buddypress/groups/single/group.php
+++ buddypress/groups/single/group.php
@@ -627,7 +627,7 @@
                                         </div>
                                     </div>
                                 </a>
-                                <a href="/groups/<?php print $group->slug; ?>/events/" class="group__events-link">
+                                <a href="/groups/<?php print $group->slug; ?>/?view=events" class="group__events-link">
                                     <?php print __('View more events', "community-portal"); ?><svg width="8" height="10" viewBox="0 0 8 10" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2.33301 8.66634L5.99967 4.99967L2.33301 1.33301" stroke="#0060DF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>
                                 </a>
                             </div>
