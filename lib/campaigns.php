2cf3bb1fb20f63cd65bd39588b8be8c7fb62af4b
Dave Naluz <dave@playgroundinc.com>
2020-02-26 15:12:13 -0500

    Updated what variables we need

--- lib/campaigns.php
+++ lib/campaigns.php
@@ -3,22 +3,20 @@
 function mozilla_mailchimp_subscribe() {
 
     if($_SERVER['REQUEST_METHOD'] === 'POST') {
-        if(isset($_POST['campaign']) && intval($_POST['campaign']) > 0) {
+        if(isset($_POST['campaign']) && strlen(trim($_POST['campaign'])) > 0) {
             $user = wp_get_current_user();
 
             // Only accepting logged in users at the moment
             if($user->ID !== 0 && isset($user->data->user_email)) {
-                $campaign = intval($_POST['campaign']);    
-                $post = get_post($campaign);
+                $campaign = trim($_POST['campaign']);    
 
-                if($post && $post->post_type === 'campaign') {
-                    $result = mozilla_add_email_to_list($campaign, $user->data->user_email);
-                    if(isset($result['id'])) {
-                        print json_encde(Array('status' =>  'OK'));
-                    }
+                $result = mozilla_add_email_to_list($campaign, $user->data->user_email);
+                if(isset($result->id)) {
+                    print json_encode(Array('status' =>  'OK'));
                 } else {
-                    print json_encode(Array('status'    =>  'ERROR', 'message'  =>  'Invalid content'));
+                    print json_encode(Array('status'    =>  'ERROR', 'message'  =>  'User not added'));
                 }
+
             } else {
                 print json_encode(Array('status'    =>  'ERROR', 'message'  =>  'Invalid user'));
             }
