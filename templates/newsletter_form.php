f9defa4b68b9056cae8770359080c301919c12aa
Ky Capstick <ky.capstick@gmail.com>
2020-03-05 18:26:51 -0500

    Add copy and responsive styles to profile signup

--- templates/newsletter_form.php
+++ templates/newsletter_form.php
@@ -4,13 +4,15 @@
 	$user_language = is_array($user_meta) && sizeof($user_meta) > 0 && isset($user_meta['languages']) && sizeof($user_meta['languages']) > 0 ? $user_meta['languages'][0] : '';
 	$user_country = is_array($user_meta) && sizeof($user_meta) > 0 && isset($user_meta['country']) ? $user_meta['country'] : '';
 ?>
-<div class="row">
-	<input type="hidden" id="fmt" name="fmt" value="H">
-	<input type="hidden" id="newsletters" name="newsletters" value="about-mozilla">
+<h2 class="profile__form-title">Get Updates</h2>
+<p>Subscribe to our newsletter and join Mozillians all around the world and learn about impactful opportunities to support Mozilla’s mission.</p>
+<input type="hidden" id="fmt" name="fmt" value="H">
+<input type="hidden" id="newsletters" name="newsletters" value="about-mozilla">
+<div class="row profile__newsletter__container">
 	<div class="col-lg-4 profile__newsletter__fields">
 		<label class="newsletter__label" for="newsletter-email">Email</label>
 		<input class="newsletter__input" aria-label="Enter your e-mail" aria-required="true" type="email" id="newsletter-email" name="newsletter-email" value="<?php echo (isset($user->user_email) && strlen($user->user_email) > 0 ? $user->user_email : '')?>">
-		<p class="newsletter__error">Invalid email.</p>
+		<span class="newsletter__error">Invalid email.</span>
 	</div>
 	<div class="col-lg-4 profile__newsletter__fields">
 		<label class="newsletter__label" for="newsletter-country">Country or Region</label>
@@ -30,12 +32,14 @@
 			<?php endforeach; ?>
 		</select>
 	</div>
-	<p class="newsletter__subtext">We’ll default to English but send in these languages whenever we can.</p>
-	<div class="cpg">
-		<input class="checkbox--hidden" type="checkbox" id="newsletter">
-		<label class="cpg__label" for="newsletter">
-			<?php echo __('Yes, subscribe me to the newsletter') ?>
-		</label>
-		<p class="newsletter__cpg__error"><?php echo __('You must agree to the privacy notice.') ?></p>
+	<div class="col-lg-12">
+		<p class="newsletter__subtext">We’ll default to English but send in these languages whenever we can.</p>
+		<div class="cpg">
+			<input class="checkbox--hidden" type="checkbox" id="newsletter">
+			<label class="cpg__label" for="newsletter">
+				<?php echo __('Yes, subscribe me to the newsletter') ?>
+			</label>
+			<p class="newsletter__cpg__error"><?php echo __('You must agree to the privacy notice.') ?></p>
+		</div>
 	</div>
 </div>
