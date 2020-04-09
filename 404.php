9a76600d15b64de3dd05811c67bc553b22757a68
Dave Naluz <dave@playgroundinc.com>
2020-04-09 09:58:05 -0400

    Removed gettext on variable

--- 404.php
+++ 404.php
@@ -8,10 +8,10 @@
             <div class="not-found__container">
                 <div class="not-found__content">
                     <div class="not-found__404">404</div>
-                    <h1 class="not-found__title"><?php if(isset($options['error_404_title']) && strlen($options['error_404_title']) > 0): ?> <?php print __($options['error_404_title']); ?><?php else: ?><?php print __('Page Not Found', 'community-portal'); ?><?php endif; ?></h1>
+                    <h1 class="not-found__title"><?php if(isset($options['error_404_title']) && strlen($options['error_404_title']) > 0): ?> <?php print $options['error_404_title']; ?><?php else: ?><?php print __('Page Not Found', 'community-portal'); ?><?php endif; ?></h1>
                     <p class="not-found__copy">
                         <?php if(isset($options['error_404_copy']) && strlen($options['error_404_copy']) > 0): ?>
-                            <?php print __($options['error_404_copy']); ?>
+                            <?php print $options['error_404_copy']; ?>
                         <?php else: ?>  
                             <?php print __('Ooops, looks like the page you are trying to reach is no longer available. If you are having trouble locating a destination on Community Portal, try visiting the home page.', 'community-portal'); ?>
                         <?php endif; ?>
