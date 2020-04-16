8aa7d94fa499d62fd664de71f1a3068cfa0beaac
Dave Naluz <dave@playgroundinc.com>
2020-04-16 17:58:47 -0400

    Fixed phpcs errors

--- index.php
+++ index.php
@@ -1,26 +1,34 @@
-<?php 
-    global $bp;
-    $group = $bp->groups->current_group;
-    $user = wp_get_current_user();
-    $meta = get_user_meta($user->ID);
+<?php
+/**
+ * Index
+ *
+ * Index file for theme
+ *
+ * @package WordPress
+ * @subpackage community-portal
+ * @version 1.0.0
+ * @author  Playground Inc.
+ */
 
-    if($group) {
-        $is_admin = groups_is_user_admin($user->ID, $group->id);
-        $edit_group = bp_is_group_admin_page() && $is_admin;
+if ( is_user_logged_in() && ( ! isset( $meta['agree'][0] ) || 'I Agree' !== $meta['agree'][0] ) ) {
+	$user = wp_get_current_user();
+	$meta = get_user_meta( $user->ID );
 
-        if($edit_group && (!isset($meta['agree'][0]) || $meta['agree'][0] != 'I Agree')) {
-            wp_redirect("/people/{$user->user_nicename}/profile/edit/group/1/");
-            die();
-        }    
-    }
-    
-    get_header(); 
+	wp_safe_redirect( "/people/{$user->user_nicename}/profile/edit/group/1/" );
+	exit();
+}
+
+
+	get_header();
 ?>
-    <div class="content">
-    <?php if ( have_posts() ) : ?>
-        <?php while ( have_posts() ) : the_post(); ?>
-          <?php the_content() ?>
-        <?php endwhile; ?>
-      <?php endif; ?>
-    </div>
+	<div class="content">
+	<?php if ( have_posts() ) : ?>
+		<?php
+		while ( have_posts() ) :
+			the_post();
+			?>
+			<?php the_content(); ?>
+		<?php endwhile; ?>
+	<?php endif; ?>
+	</div>
 <?php get_footer(); ?>
