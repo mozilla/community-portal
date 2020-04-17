69d78154b9330391bc0ae7745a9b3974669d0928
Dave Naluz <dave@playgroundinc.com>
2020-04-16 22:13:02 -0400

    Passes phpcs wordpress standards

--- index.php
+++ index.php
@@ -2,22 +2,36 @@
 /**
  * Index
  *
- * Index file for theme
+ * Main footer file for the theme.
  *
  * @package WordPress
  * @subpackage community-portal
  * @version 1.0.0
  * @author  Playground Inc.
+ * @license https://www.gnu.org/licenses/gpl-3.0.txt GNU/GPLv3
+ * @since  1.0.0
  */
 
-if ( is_user_logged_in() && ( ! isset( $meta['agree'][0] ) || 'I Agree' !== $meta['agree'][0] ) ) {
-	$user = wp_get_current_user();
-	$meta = get_user_meta( $user->ID );
+global $bp;
+$group = $bp->groups->current_group;
+$user  = wp_get_current_user();
+$meta  = get_user_meta( $user->ID );
 
-	wp_safe_redirect( "/people/{$user->user_nicename}/profile/edit/group/1/" );
-	exit();
+
+if ( $group ) {
+	$is_admin   = groups_is_user_admin( $user->ID, $group->id );
+	$edit_group = bp_is_group_admin_page() && $is_admin;
+
+	if ( $edit_group && ( ! isset( $meta['agree'][0] ) || 'I Agree' !== $meta['agree'][0] ) ) {
+		wp_safe_redirect( "/people/{$user->user_nicename}/profile/edit/group/1/" );
+		exit();
+	}
+} else {
+	if ( ( ! isset( $meta['agree'][0] ) || 'I Agree' !== $meta['agree'][0] ) ) {
+		wp_safe_redirect( "/people/{$user->user_nicename}/profile/edit/group/1/" );
+		exit();
+	}
 }
-
 
 	get_header();
 ?>
