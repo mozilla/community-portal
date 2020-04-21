f97a79564161f5023cbd8a6f604fbd5d086232e1
Ky Capstick <ky.capstick@gmail.com>
2020-04-21 15:32:56 -0400

    Update text card block to abide by code sniffing

--- templates/blocks/text-card-block.php
+++ templates/blocks/text-card-block.php
@@ -1,10 +1,19 @@
 <div class="campaign__text-card-block">
-    <div class="campaign__block-container<?php if($block['keyline']): ?> campaign__block-container--keyline<?php endif; ?>">
-        <h2 class="campaign__heading-2"><?php print $block['title']; ?></h2>
-        <div class="campaign__block-content">
-            <div class="campaign__card">
-                <?php print $block['copy']; ?>
-            </div>
-        </div>
-    </div>
+	<div class="campaign__block-container<?php if ( $block['keyline'] ) :
+		?> campaign__block-container--keyline<?php endif; ?>">
+		<h2 class="campaign__heading-2"><?php print esc_html( $block['title'] ); ?></h2>
+		<div class="campaign__block-content">
+			<div class="campaign__card">
+				<?php print wp_kses(
+						wpautop( substr( trim( $block['copy'] ), 0, 3000 ) ),
+						array(
+							'p'  => array(),
+							'br' => array(),
+							'ul' => array(),
+							'li' => array(),
+						)
+					); ?>
+			</div>
+		</div>
+	</div>
 </div>
