2a15fbdf4d6a8fda9a50f8294dca19a1bb28df94
Ky Capstick <ky.capstick@gmail.com>
2020-04-21 15:34:58 -0400

    Update text card block to abide by code sniffing

--- templates/blocks/text-card-block.php
+++ templates/blocks/text-card-block.php
@@ -1,18 +1,36 @@
+<?php
+/**
+ * Text 3up Block - Campaigns
+ *
+ * Text 3up block for campaigns
+ *
+ * @package WordPress
+ * @subpackage community-portal
+ * @version 1.0.0
+ * @author  Playground Inc.
+ */
+
+?>
 <div class="campaign__text-card-block">
-	<div class="campaign__block-container<?php if ( $block['keyline'] ) :
-		?> campaign__block-container--keyline<?php endif; ?>">
+	<div class="campaign__block-container
+	<?php
+	if ( $block['keyline'] ) :
+		?>
+		campaign__block-container--keyline<?php endif; ?>">
 		<h2 class="campaign__heading-2"><?php print esc_html( $block['title'] ); ?></h2>
 		<div class="campaign__block-content">
 			<div class="campaign__card">
-				<?php print wp_kses(
-						wpautop( substr( trim( $block['copy'] ), 0, 3000 ) ),
-						array(
-							'p'  => array(),
-							'br' => array(),
-							'ul' => array(),
-							'li' => array(),
-						)
-					); ?>
+				<?php
+				print wp_kses(
+					wpautop( substr( trim( $block['copy'] ), 0, 3000 ) ),
+					array(
+						'p'  => array(),
+						'br' => array(),
+						'ul' => array(),
+						'li' => array(),
+					)
+				);
+				?>
 			</div>
 		</div>
 	</div>
