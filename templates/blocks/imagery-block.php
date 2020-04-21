1e509362e90befbaf68dce4131774740367b2577
Ky Capstick <ky.capstick@gmail.com>
2020-04-21 14:20:33 -0400

    Update 1up block to abide by code sniffing standards

--- templates/blocks/imagery-block.php
+++ templates/blocks/imagery-block.php
@@ -29,7 +29,9 @@ if ( 'grey' === $block['background_color'] ) :
 				wpautop( substr( trim( $block['copy'] ), 0, 3000 ) ),
 				array(
 					'p'  => array(),
-					'br' => array(),
+          'br' => array(),
+          'ul' => array(),
+          'li' => array()
 				)
 			);
 
--- templates/blocks/text-1up-block.php
+++ templates/blocks/text-1up-block.php
@@ -1,11 +1,50 @@
-<div class="campaign__1up-text-block<?php if($block['background_color'] === 'grey'): ?> campaign__1up-text-block--grey<?php endif; ?>">
-    <div class="campaign__block-container<?php if($block['keyline']): ?> campaign__block-container--keyline<?php endif; ?>">
-        <h2 class="campaign__heading-2"><?php print $block['title']; ?></h2>
-        <div class="campaign__block-content">
-            <?php print $block['copy']; ?>
-        </div>
-        <?php if($block['cta']): ?>
-            <a href="<?php if($block['cta_link']): ?> <?php print $block['cta_link']; ?><?php else: ?>#<?php endif; ?>" class="campaign__cta"><?php print $block['cta']; ?></a>
-        <?php endif; ?>
-    </div>
+<?php
+/**
+ * Text 1up Block - Campaigns
+ *
+ * Text 1up block for campaigns
+ *
+ * @package WordPress
+ * @subpackage community-portal
+ * @version 1.0.0
+ * @author  Playground Inc.
+ */
+
+?>
+<div class="campaign__1up-text-block
+<?php
+if ( 'grey' === $block['background_color'] ) :
+	?>
+	campaign__1up-text-block--grey<?php endif; ?>">
+	<div class="campaign__block-container
+	<?php
+	if ( $block['keyline'] ) :
+		?>
+		campaign__block-container--keyline<?php endif; ?>">
+		<h2 class="campaign__heading-2"><?php print esc_html( $block['title'] ); ?></h2>
+		<div class="campaign__block-content">
+			<?php
+			print wp_kses(
+				wpautop( substr( trim( $block['copy'] ), 0, 3000 ) ),
+				array(
+					'p'  => array(),
+					'br' => array(),
+					'ul' => array(),
+					'li' => array(),
+				)
+			);
+			?>
+		</div>
+		<?php if ( $block['cta'] ) : ?>
+			<a href="
+			<?php
+			if ( $block['cta_link'] ) :
+				?>
+					<?php print esc_attr( $block['cta_link'] ); ?>
+				<?php
+else :
+	?>
+				#<?php endif; ?>" class="campaign__cta"><?php print esc_html( $block['cta'] ); ?></a>
+		<?php endif; ?>
+	</div>
 </div>
