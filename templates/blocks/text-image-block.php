e86f9497de1fa0a0abb5df9fa03dfcdc038148b2
Ky Capstick <ky.capstick@gmail.com>
2020-04-21 15:44:55 -0400

    Update text-image cards to be compliant with code sniffing standards

--- templates/blocks/text-image-block.php
+++ templates/blocks/text-image-block.php
@@ -1,15 +1,46 @@
-<div class="campaign__image-text-block<?php if($block['background_color'] === 'grey'): ?> campaign__image-text-block--grey<?php endif; ?>">
-    <div class="campaign__block-container<?php if($block['keyline']): ?> campaign__block-container--keyline<?php endif; ?>">
-        <div class="campaign__block-content">
-            <div class="campaign__image-text-block-container">
-                <div class="campaign__image-text-block-text-container">
-                    <h2 class="campaign__heading-2"><?php print $block['title']; ?></h2>
-                    <?php print $block['copy']; ?>
-                </div>
-                <div class="campaign__image-text-block-image-container">
-                    <img src="<?php print $block['image']['url']; ?>"  class="campaign__image-text-block-image"/>
-                </div>
-            </div>
-        </div>
-    </div>
+<?php
+/**
+ * Text Image Block - Campaigns
+ *
+ * Text Image block for campaigns
+ *
+ * @package WordPress
+ * @subpackage community-portal
+ * @version 1.0.0
+ * @author  Playground Inc.
+ */
+
+?>
+<div class="campaign__image-text-block
+<?php
+if ( 'grey' === $block['background_color'] ) :
+	?>
+	campaign__image-text-block--grey<?php endif; ?>">
+	<div class="campaign__block-container
+	<?php
+	if ( $block['keyline'] ) :
+		?>
+		campaign__block-container--keyline<?php endif; ?>">
+		<div class="campaign__block-content">
+			<div class="campaign__image-text-block-container">
+				<div class="campaign__image-text-block-text-container">
+					<h2 class="campaign__heading-2"><?php print esc_html( $block['title'] ); ?></h2>
+					<?php
+					print wp_kses(
+						wpautop( substr( trim( $block['copy'] ), 0, 3000 ) ),
+						array(
+							'p'  => array(),
+							'br' => array(),
+							'ul' => array(),
+							'li' => array(),
+						)
+					);
+					?>
+				</div>
+				<div class="campaign__image-text-block-image-container">
+					<img src="<?php print esc_attr( $block['image']['url'] ); ?>"  class="campaign__image-text-block-image"/>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
