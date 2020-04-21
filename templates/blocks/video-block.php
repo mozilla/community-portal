8b36884548430f416d69ee2dccbb96d476229960
Ky Capstick <ky.capstick@gmail.com>
2020-04-21 15:51:27 -0400

    Update code for campaign video blocks to meet standards of code sniffer

--- templates/blocks/video-block.php
+++ templates/blocks/video-block.php
@@ -1,20 +1,59 @@
-<div class="campaign__video-block<?php if($block['background_color'] === 'grey'): ?> campaign__video-block--grey<?php endif; ?>">
-    <div class="campaign__block-container<?php if($block['keyline']): ?> campaign__block-container--keyline<?php endif; ?>">
-        <h2 class="campaign__heading-2"><?php print $block['title']; ?></h2>
-        <div class="campaign__block-content">
-            <?php print $block['copy']; ?>
-        </div>
-        <?php if($block['cta']): ?>
-            <a href="<?php if($block['cta_link']): ?> <?php print $block['cta_link']; ?><?php else: ?>#<?php endif; ?>" class="campaign__cta"><?php print $block['cta']; ?></a>
-        <?php endif; ?>
-        <?php 
-            preg_match('/[\\?\\&]v=([^\\?\\&]+)/', $block['video'], $matches);
-            $youtube_id = (is_array($matches) && sizeof($matches) > 1) ? $matches[1] : false;
-        ?>
-        <?php if($youtube_id): ?>
-        <div class="campaign__video-container">
-            <iframe class="campaign__video" src="https://www.youtube.com/embed/<?php print $youtube_id; ?>"></iframe>
-        </div>
-        <?php endif; ?>
-    </div>
+<?php
+/**
+ * Video Block - Campaigns
+ *
+ * Video block for campaigns
+ *
+ * @package WordPress
+ * @subpackage community-portal
+ * @version 1.0.0
+ * @author  Playground Inc.
+ */
+
+?>
+<div class="campaign__video-block
+<?php
+if ( 'grey' === $block['background_color'] ) :
+	?>
+	campaign__video-block--grey<?php endif; ?>">
+	<div class="campaign__block-container
+	<?php
+	if ( $block['keyline'] ) :
+		?>
+		campaign__block-container--keyline<?php endif; ?>">
+		<h2 class="campaign__heading-2"><?php print esc_html( $block['title'] ); ?></h2>
+		<div class="campaign__block-content">
+		<?php
+			print wp_kses(
+				wpautop( substr( trim( $block['copy'] ), 0, 3000 ) ),
+				array(
+					'p'  => array(),
+					'br' => array(),
+					'ul' => array(),
+					'li' => array(),
+				)
+			);
+			?>
+		</div>
+		<?php if ( $block['cta'] ) : ?>
+			<a href="
+			<?php
+			if ( $block['cta_link'] ) :
+				?>
+				<?php print esc_attr( $block['cta_link'] ); ?>
+				<?php
+else :
+	?>
+				#<?php endif; ?>" class="campaign__cta"><?php print esc_html( $block['cta'] ); ?></a>
+		<?php endif; ?>
+		<?php
+			preg_match( '/[\\?\\&]v=([^\\?\\&]+)/', $block['video'], $matches );
+			$youtube_id = ( is_array( $matches ) && count( $matches ) > 1 ) ? $matches[1] : false;
+		?>
+		<?php if ( $youtube_id ) : ?>
+		<div class="campaign__video-container">
+			<iframe class="campaign__video" src="https://www.youtube.com/embed/<?php print esc_attr( $youtube_id ); ?>"></iframe>
+		</div>
+		<?php endif; ?>
+	</div>
 </div>
