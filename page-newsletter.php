79494bf309c9b6255da10f79152c5c1a72130b40
Ky Capstick <ky.capstick@gmail.com>
2020-04-22 10:40:13 -0400

    Updating filename for campaigns newsletter

--- page-newsletter.php
+++ page-newsletter.php
@@ -2,7 +2,7 @@
 	get_header();
 ?>
 	<div class="newsletter newsletter__page">
-		<?php include get_template_directory()."/templates/campaigns_newsletter.php"; ?>
+		<?php include get_template_directory()."/templates/campaigns-newsletter.php"; ?>
 	</div>
 <?php 
 	get_footer();
