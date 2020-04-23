1adc8d032db02bf97256f97643092f416f8f15ac
Ky Capstick <ky.capstick@gmail.com>
2020-04-23 11:56:38 -0400

    Reviewing all updated files for compliance with Code Sniffer standards

--- page-newsletter.php
+++ page-newsletter.php
@@ -1,9 +1,23 @@
-<?php 
+<?php
+/**
+ * Newsletter
+ *
+ * Individual page for users to sign up for the newsletter
+ *
+ * @package    WordPress
+ * @subpackage community-portal
+ * @version    1.0.0
+ * @author     Playground Inc.
+ */
+
+?>
+
+<?php
 	get_header();
 ?>
 	<div class="newsletter newsletter__page">
-		<?php include get_template_directory()."/templates/campaigns-newsletter.php"; ?>
+		<?php require get_template_directory() . '/templates/campaigns-newsletter.php'; ?>
 	</div>
-<?php 
+<?php
 	get_footer();
 ?>
--- templates/campaign-rsvp.php
+++ templates/campaign-rsvp.php
@@ -68,7 +68,7 @@
 						<input class="btn btn--light btn--submit" type="submit" value="<?php esc_html_e( 'Get Involved', 'community-portal' ); ?>">
 					</div>
 				</div>
-				<?php wp_nonce_field('mailing-list'); ?>
+				<?php wp_nonce_field( 'mailing-list' ); ?>
 			</form>
 		</div>
 	</div>
