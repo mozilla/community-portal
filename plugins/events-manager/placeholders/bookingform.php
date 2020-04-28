826d793760257460a962a4e38d0dd7ae8eda7fd3
Ky Capstick <ky.capstick@gmail.com>
2020-04-28 09:12:23 -0400

    clean up as many of the errors as possible in the events folder

--- plugins/events-manager/placeholders/bookingform.php
+++ plugins/events-manager/placeholders/bookingform.php
@@ -33,19 +33,21 @@ if ( get_option( 'dbem_css_rsvp' ) ) {
 		$em_booking->cancel();
 		$em_booking->delete();
 
-		$updated_url = remove_query_arg( 'cancel', $_SERVER['REQUEST_URI'] );
+		if ( isset( $_SERVER['REQUEST_URI'] ) ) {
+			$updated_url = remove_query_arg( 'cancel', esc_url_raw( wp_unslash( $_SERVER['REQUEST_URI'] ) ) );
+		}
 
 		?>
 		<script type="text/javascript">
-			window.history.replaceState("","", "<?php echo $updated_url ; ?>")
+			window.history.replaceState("","", "<?php echo esc_attr( $updated_url ); ?>")
 		</script>
 		<?php
 		$em_booking = $em_event->get_bookings()->has_booking();
 	}
 	?>
 
-	<?php if ( is_object( $em_booking ) ) : ?>
-	<a class="em-bookings-cancel events-single__cancel btn btn--submit btn--dark" href="<?php echo esc_attr( add_query_arg( array( 'cancel' => true ), $_SERVER['REQUEST_URI'] ) ); ?>" onclick="if( !confirm('<?php esc_html_e( 'Are you sure you dont want to attend this event?', 'community-portal' ); ?>') ){ return false; }">
+	<?php if ( is_object( $em_booking ) && isset( $_SERVER['REQUEST_URI'] ) ) : ?>
+	<a class="em-bookings-cancel events-single__cancel btn btn--submit btn--dark" href="<?php echo esc_attr( add_query_arg( array( 'cancel' => true ), esc_url_raw( wp_unslash( $_SERVER['REQUEST_URI'] ) ) ) ); ?>" onclick="if( !confirm('<?php esc_html_e( 'Are you sure you dont want to attend this event?', 'community-portal' ); ?>') ){ return false; }">
 		<?php esc_html_e( 'Will Not Attend', 'community-portal' ); ?>
 	</a>
 	<?php else : ?>
