70d2acd95dc2a862e78f5ed6b1ece8a64fe8c4e5
Dave Naluz <dave@playgroundinc.com>
2020-05-12 10:18:02 -0400

    Fixed bug with returning activities / campaigns

--- templates/event-export.php
+++ templates/event-export.php
@@ -12,13 +12,14 @@
 
 	$args = array(
 		'post_type' => 'campaign',
-		'per_page'  => -1,
+		'post_per_page'  => -1,
 	);
 
 	$campaigns = new WP_Query( $args );
 
 	$args = array(
 		'post_type' => 'activity',
+		'post_per_page'  => -1,
 	);
 
 	$activities = new WP_Query( $args );
