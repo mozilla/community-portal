4130f9a77d96513de19cf2111e124142f03b3ad6
Jenny <jenny@playgroundinc.com>
2020-06-01 14:30:32 -0400

    Add doc and function comments, yoda condition, and check for array index before access

--- lib/newsletter.php
+++ lib/newsletter.php
@@ -1,7 +1,20 @@
 <?php
+/**
+ * Newsletter Library
+ *
+ * Newsletter Functionality
+ *
+ * @package    WordPress
+ * @subpackage community-portal
+ * @version    1.0.0
+ * @author     Playground Inc.
+ */
 
+/**
+ * Newsletter Subscribe
+ **/
 function mozilla_newsletter_subscribe() {
-	if ( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
+	if ( ! empty( $_SERVER['REQUEST_METHOD'] ) && 'POST' === $_SERVER['REQUEST_METHOD'] ) {
 		$user = wp_get_current_user();
 		if ( isset( $user ) ) {
 			if ( isset( $_POST['subscribed'] ) ) {
