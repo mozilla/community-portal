f00da9472f9d2c644d365979d3f70183a8fd0056
Dave Naluz <dave@playgroundinc.com>
2020-06-01 16:20:46 -0400

    Bug fix

--- lib/newsletter.php
+++ lib/newsletter.php
@@ -17,7 +17,7 @@ function mozilla_newsletter_subscribe() {
 	if ( ! empty( $_SERVER['REQUEST_METHOD'] ) && 'POST' === $_SERVER['REQUEST_METHOD'] ) {
 		$user = wp_get_current_user();
 		if ( isset( $user ) ) {
-			if ( isset( $_POST['subscribed'] ) && isset( $_POST['newsletter_nonce_field'] ) && wp_verify_nonce( sanitize_text_field( wp_unslash( $_POST['newsletter_nonce_field'] ) ), 'newsletter_nonce' ) ) {
+			if ( isset( $_POST['subscribed'] ) && isset( $_POST['newsletter_nonce'] ) && wp_verify_nonce( sanitize_text_field( wp_unslash( $_POST['newsletter_nonce'] ) ), 'newsletter_nonce' ) ) {
 				$subscribed = intval( $_POST['subscribed'] );
 				update_user_meta( $user->ID, 'newsletter', $subscribed );
 			}
