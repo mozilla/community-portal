5266eb6a276df159b75acd789c0b9ea24fd16d99
Ky Capstick <ky.capstick@gmail.com>
2020-07-02 10:16:10 -0400

    Update event tags functions to assume post tags due to new action

--- lib/utils.php
+++ lib/utils.php
@@ -491,7 +491,7 @@ function mozilla_add_query_vars_filter( $vars ) {
  * @param int $tt_id id for taxonomy.
  */
 function mozilla_create_event_category( $term_id, $tt_id ) {
-	$term = get_term( $term_id, $taxonomy );
+	$term = get_term( $term_id, 'post_tags' );
 	if ( !empty( $term ) && false === stripos( $term->slug, '_' ) ) {
 		wp_insert_term( $term->name, 'event-categories', array( 'slug' => $term->slug ) );
 	}
@@ -507,7 +507,7 @@ add_action( 'create_post_tag', 'mozilla_create_event_category', 10, 2 );
  * @param int $tt_id id for taxonomy.
  */
 function mozilla_update_event_category( $term_id, $tt_id ) {
-	$term     = get_term( $term_id, $taxonomy );
+	$term     = get_term( $term_id, 'post_tags' );
 	$cat_term = get_term_by( 'slug', $term->slug, 'event-categories' );
 	if ( empty( $cat_term ) ) {
 		$cat_term = get_term_by( 'name', $term->name, 'event-categories' );
