c57dde47ac4fd35c729a18cd1ff9c41321a4860e
Daniele Scasciafratte <mte90net@gmail.com>
2022-03-25 18:00:48 +0100

    fix(event): force rsvp date, #769

--- functions.php
+++ functions.php
@@ -250,7 +250,8 @@ function remove_wp_block_library_css() {
 add_action( 'wp_enqueue_scripts', 'remove_wp_block_library_css', 100 );
 
 // em-event.php 585: apply_filters( 'em_event_load_postdata', $this );.
-/*
+
+/**
  * Force RSVP date as the end date
  */
 add_filter( 'em_event_load_postdata', 'mozilla_force_rsvp_end', 10, 2 );
