2ca6f0ec8e162a843b9531337621e347788e94a1
Daniele Scasciafratte <mte90net@gmail.com>
2022-04-06 17:47:37 +0200

    feat(event): rsvp in 6 months

--- functions.php
+++ functions.php
@@ -257,7 +257,7 @@ add_action( 'wp_enqueue_scripts', 'remove_wp_block_library_css', 100 );
  * @param object $event EM Event.
  */
 function mozilla_force_rsvp_end( $event ) {
-	$event->event_rsvp_date = gmdate( 'd/m/Y', strtotime( "+6 months", strtotime( $event->event_end_date ) ) );
+	$event->event_rsvp_date = gmdate( 'd/m/Y', strtotime( '+6 months', strtotime( $event->event_end_date ) ) );
 	return $event;
 }
 add_filter( 'em_event_load_postdata', 'mozilla_force_rsvp_end', 10, 2 );
